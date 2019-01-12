42c1743ab57deb6deca1ec3bdfa2643f2329c39e
emiliano-ponce <emilianoponce01@gmail.com>
2019-01-12 12:37:09 -0600

    Add eslint and standard config

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 
 const rootUrl = 'http://localhost:3001/api'
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div>
 
       </div>
-    );
-    }
+    )
   }
+}
 
-export default App;
+export default App
